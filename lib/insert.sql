0a51cc5f7c5cb9c99bb119955d9124efa7fea483
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-08-23 17:07:30 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -24,7 +24,3 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES (null, 8, 'M', 'brown', 'stubborn', 1);
-
-
-
-
